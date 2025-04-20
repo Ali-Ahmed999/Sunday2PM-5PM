5dd71679a8c53ccaf019b8d2f953ba25446cf7fd
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-04-20 15:03:29 +0500

    Rivision of list

---
+++ class06_revisions/index.py
@@ -0,0 +1,11 @@
+# List 
+months: list = ['January' , 'Febuary', 'March', 'April', 'May', 'June', 'July', 'Aug']
+
+# print(months[0:3])
+# Slicing List 
+print(months[3:6])
+
+print(months[3:])
+
+# Negative Indexing
+print(months[-3:-1])
